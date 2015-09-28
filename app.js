f97d544ca595112f995da9e7b309c3aedf871405
Patrick Nguyen <superpatricko@gmail.com>
2015-09-28 17:14:33 -0400

    Add body-parser
    To allow us to read the body of a request, even if the body of the
    request is in JSON

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://localhost/bookapi');
 var Book = require('./models/bookModel');
 var app = express();
 var port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json); // allows reading bodies or requests
 
 var bookRouter = express.Router();
 
 bookRouter.route('/Books')
+	.post(function(req, res) {
+		var book = new Book(req.body);
+		console.log(book);
+		res.send(book);
+	})
 	.get(function(req, res) {
 		var query = {};
 
